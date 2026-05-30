8e6f65089087ade4426ded3ec9beec9b784e952e
xiaochun <90xchun@gmail.com>
2026-05-30 12:18:30 +0800

    Update to vue3.x

--- src/main/java/com/zrlog/plugin/cos/Application.java
+++ src/main/java/com/zrlog/plugin/cos/Application.java
@@ -7,6 +7,7 @@ import com.zrlog.plugin.cos.handler.ConnectHandler;
 import com.zrlog.plugin.cos.service.CosStaticSyncService;
 import com.zrlog.plugin.cos.service.UploadService;
 import com.zrlog.plugin.cos.service.UploadToPrivateService;
+import com.zrlog.plugin.render.SimpleTemplateRender;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -20,7 +21,7 @@ public class Application {
     public static void main(String[] args) throws IOException {
         List<Class<?>> classList = new ArrayList<>();
         classList.add(CosController.class);
-        new NioClient(new ConnectHandler(), null)
+        new NioClient(new ConnectHandler(), new SimpleTemplateRender())
                 .connectServer(args, classList, CosPluginAction.class,
                         Arrays.asList(UploadService.class, UploadToPrivateService.class, CosStaticSyncService.class));
     }
